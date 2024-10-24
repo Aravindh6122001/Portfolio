9c188eba387a9418eba9fe7c82747300731c0ccc
Aravindh6122001 <aravindhkrishna1922@gmail.com>
2024-10-24 13:34:41 +0530

    preCommit changes

--- FE_Client/vite.config.ts
+++ FE_Client/vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // Vite configuration
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist", // This will specify the output directory
-    emptyOutDir: true, // Clear the dist folder before building
-  },
 });
