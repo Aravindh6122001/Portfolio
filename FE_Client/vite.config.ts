3845ff8e6cd46ddb4c5f6daa41c2a0211b599cee
Aravindh6122001 <aravindhkrishna1922@gmail.com>
2024-10-24 13:19:44 +0530

    Stage!_preBuildfixes added

--- FE_Client/vite.config.ts
+++ FE_Client/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // This will specify the output directory
+    emptyOutDir: true, // Clear the dist folder before building
+  },
+});
